3f49d983c6f67aace2ce5bf05d86bf7313dc8078
sadelcarpio <sadelcarpioa@gmail.com>
2024-11-16 20:08:13 -0500

    Initialized uv projects on experiments with common library of functions

---
+++ experiments/libs/libs/utils.py
@@ -0,0 +1,2 @@
+def dummy_data():
+    return [1, 2, 3]
---
+++ experiments/tf_models/hello.py
@@ -0,0 +1,10 @@
+from libs.utils import dummy_data
+
+
+def main():
+    print("Hello from tf-models!")
+    print(dummy_data())
+
+
+if __name__ == "__main__":
+    main()
