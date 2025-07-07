5a9c15767d1fce3192f6dd21e19dd2f21131c5e7
sadelcarpio <sadelcarpioa@gmail.com>
2025-07-06 20:57:22 -0500

    reformat games table

--- dbt/models/staging/stg_games.sql
+++ dbt/models/staging/stg_games.sql
@@ -5,18 +5,20 @@ WITH filtered AS (SELECT *
                   FROM {{ source('raw', 'raw_games') }}
                   WHERE type = 'game'
                     AND coming_soon = false)
-SELECT appid               AS game_id,
-       name                AS game_name,
-       developers          AS game_developers,          -- list
-       publishers          AS game_publishers,          -- list
-       categories          AS game_categories,          -- list
-       genres              AS game_genres,              -- list
+SELECT appid                  AS game_id,
+       name                   AS game_name,
+       developers             AS game_developers, -- list
+       publishers             AS game_publishers, -- list
+       categories             AS game_categories, -- list
+       genres                 AS game_genres,     -- list
        COALESCE(
                TRY_STRPTIME(release_date, '%b %d, %Y'),
                TRY_STRPTIME(release_date, '%d %b, %Y')
-       )                   AS game_release_date,
-       short_description   AS game_short_description,
-       supported_languages AS game_supported_languages, -- list
-       review_score        AS game_review_score,
-
+       )                      AS game_release_date,
+       short_description      AS game_short_description,
+       review_score           AS game_review_score,
+       review_score_desc      AS game_review_score_description,
+       total_reviews          AS game_total_reviews,
+       total_positive_reviews AS game_total_positive_reviews,
+       total_negative_reviews AS game_total_negative_reviews
 FROM filtered
