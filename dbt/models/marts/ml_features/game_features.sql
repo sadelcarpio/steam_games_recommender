805dbe596f84663191c70e7f5a68463edfeadf60
sadelcarpio <sadelcarpioa@gmail.com>
2025-08-26 23:27:29 -0500

    avoid duplicate game_index. TODO EDA

--- dbt/models/marts/ml_features/game_features.sql
+++ dbt/models/marts/ml_features/game_features.sql
@@ -2,5 +2,10 @@
 {{ config(
     materialized='table'
 ) }}
-SELECT g.*, a.*
+SELECT g.*,
+       a.game_review_day,
+       a.game_num_reviews
+       a.game_num_positive_reviews,
+       a.game_num_negative_reviews,
+       a.game_weighted_score
 FROM {{ ref('daily_game_metrics') }} a JOIN {{ ref('dim_games') }} g using (game_index)
