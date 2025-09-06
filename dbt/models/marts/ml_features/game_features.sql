54ad89625f70f49ee4c617e8b52412ef06945c43
sadelcarpio <sadelcarpioa@gmail.com>
2025-09-06 18:08:11 -0500

    Refactor data source configuration to use external locations and formats, update incremental model logic, and remove DuckDB initialization script.

--- dbt/models/marts/ml_features/game_features.sql
+++ dbt/models/marts/ml_features/game_features.sql
@@ -1,11 +1,19 @@
 -- models/marts/ml_features/game_features.sql
 {{ config(
-    materialized='table'
+    materialized='incremental',
+    unique_key=['game_id', 'game_review_day']
 ) }}
+WITH filtered_game_metrics AS (
+    SELECT *
+    FROM {{ ref('daily_game_metrics') }}
+    {% if is_incremental %}
+    WHERE game_review_day >= (SELECT MAX(game_review_day) FROM {{ this }})
+    {% endif %}
+)
 SELECT g.*,
        a.game_review_day,
        a.game_num_reviews,
        a.game_num_positive_reviews,
        a.game_num_negative_reviews,
        a.game_weighted_score
-FROM {{ ref('daily_game_metrics') }} a JOIN {{ ref('dim_games') }} g using (game_index)
+FROM filtered_game_metrics a JOIN {{ ref('dim_games') }} g using (game_index)
